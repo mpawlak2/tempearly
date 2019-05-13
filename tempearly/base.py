dceadfb81957aa378fbd272023735334b1399de6
Mateusz Pawlak <mpawlak62@gmail.com>
2019-05-13 11:25:01 +0200

    Create Token class

--- tempearly/base.py
+++ tempearly/base.py
@@ -10,7 +10,7 @@ import re
 VARIABLE_TAG_START = "*"
 VARIABLE_TAG_END = "*"
 
-tags_re = re.compile(r"{}.*?{}".format(
+tags_re = re.compile(r"({}.*?{})".format(
 	re.escape(VARIABLE_TAG_START), re.escape(VARIABLE_TAG_END),
 ))
 
@@ -22,7 +22,18 @@ class Template():
 
 	def render(self):
 		"""Render template string"""
-		return self.template
+		contents = []
+		rendered = self.template
+
+		for token in tags_re.split(rendered):
+			if token.startswith(VARIABLE_TAG_START):
+				start_l = len(VARIABLE_TAG_START)
+				end_l = len(VARIABLE_TAG_END)
+
+				token = Token(token[start_l:-end_l])
+			contents.append(token)
+
+		return "".join([str(e) for e in contents])
 
 	@classmethod
 	def from_string(cls, template, context=None):
@@ -37,3 +48,22 @@ class Template():
 		if not context:
 			context = {}
 		return cls(template, context)
+
+
+class Token:
+	"""Represents inline token
+
+	After parsing the whole string, Template class will create tokens to
+	represent parts of the code that require additional processing.
+
+	Types of tokens:
+	VARIABLE - represents a variable token, for example, *VAR* would translate
+	to Token instance with VARIABLE type.
+
+	"""
+
+	def __init__(self, content):
+		self.content = content
+
+	def __str__(self):
+		return self.content
