bc58fcf565f70cb48a99eda9ca75ec5a3f67685b
Mateusz Pawlak <mpawlak62@gmail.com>
2019-05-14 12:37:37 +0200

    Add docstrings
    Explain default variables expansion.

--- tempearly/defaults.py
+++ tempearly/defaults.py
@@ -5,17 +5,46 @@ a template string.
 import datetime
 
 DEFAULT_VARIABLE_REGISTRY = {}
+_KEYS = set()
 
 
 class DefaultVariableMeta(type):
+    """Metaclass that will register default fields."""
+
     def __new__(meta, name, bases, class_dict):
+        """There are some conditions:
+
+        (1) The class_dict must contain `name` key.
+        (2) The `key` must be unique
+        """
         cls = type.__new__(meta, name, bases, class_dict)
         if len(bases) > 0:
-            DEFAULT_VARIABLE_REGISTRY[class_dict["name"]] = cls()
+            if "name" not in class_dict:
+                raise AttributeError(
+                    "You must provide the `name` attribute on your custom"
+                    " default variable class"
+                )
+
+            default_var_name = class_dict["name"]
+            if default_var_name in _KEYS:
+                raise AttributeError(
+                    f"The default variable with the name `{default_var_name}` already"
+                    " exists, choose another name."
+                )
+
+            _KEYS.add(default_var_name)
+            DEFAULT_VARIABLE_REGISTRY[default_var_name] = cls()
         return cls
 
 
 class DefaultVariable(metaclass=DefaultVariableMeta):
+    """When subclassing always remember to provide the `name` class attribute
+    which must be a unique variable name (among default variables that are already defined).
+
+    Usage from a template string:
+        From a template string you would use your variable by prefixing it with the
+        `D` character, i.e., capital D.
+    """
     pass
 
 
@@ -25,3 +54,10 @@ class DDate(DefaultVariable):
 
     def __call__(self):
         return datetime.date.today()
+
+
+class DDatetime(DefaultVariable):
+    name = "datetime"
+
+    def __call__(self):
+        return datetime.datetime.today()
