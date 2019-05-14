36721bd5c07ebf155cfb908a7c6d4d724f28cd82
Mateusz Pawlak <mpawlak62@gmail.com>
2019-05-14 20:08:25 +0200

    Convert tabs to spaces

--- tempearly/base.py
+++ tempearly/base.py
@@ -26,165 +26,165 @@ VARIABLE_TAG_START = "<<"
 VARIABLE_TAG_END = ">>"
 
 tags_re = re.compile(r"({}.*?{})".format(
-	re.escape(VARIABLE_TAG_START), re.escape(VARIABLE_TAG_END),
+    re.escape(VARIABLE_TAG_START), re.escape(VARIABLE_TAG_END),
 ))
 
 
 def create_exception(message, token=None, exception_class=TemplateSyntaxError):
-	"""Create an exception instance; a helper function.
+    """Create an exception instance; a helper function.
 
-	Arguments:
-		token
-			is the Token object, representing the match in a template string. (None by default)
+    Arguments:
+        token
+            is the Token object, representing the match in a template string. (None by default)
 
-		exception_class
-			is a type of the TemplateError exception class, e.g., create_exception or TemplateKeyError
-	"""
-	return exception_class(message, token=token)
+        exception_class
+            is a type of the TemplateError exception class, e.g., create_exception or TemplateKeyError
+    """
+    return exception_class(message, token=token)
 
 
 class Template():
-	def __init__(self, template, context):
-		"""Represents a template string."""
-		self.template = template
-		self.context = context
-		self.tokens = []
+    def __init__(self, template, context):
+        """Represents a template string."""
+        self.template = template
+        self.context = context
+        self.tokens = []
 
-	def process_token(self, token):
-		"""Process a token and return rendered value.
+    def process_token(self, token):
+        """Process a token and return rendered value.
 
-		A token can be either string literal or Token instance,
-		detect which is it and process it accordingly.
-		"""
-		if isinstance(token, Token):
-			return str(token.render(self.context))
-		return str(token)
+        A token can be either string literal or Token instance,
+        detect which is it and process it accordingly.
+        """
+        if isinstance(token, Token):
+            return str(token.render(self.context))
+        return str(token)
 
-	def tokenize(self):
-		"""Generate a token list from the input string.
-		
-		When finished, the `self.tokens` attribute will be populated.
-		"""
-		tokens = []
-		rendered = self.template
-		line_no = 1
+    def tokenize(self):
+        """Generate a token list from the input string.
+        
+        When finished, the `self.tokens` attribute will be populated.
+        """
+        tokens = []
+        rendered = self.template
+        line_no = 1
 
-		for token in tags_re.split(rendered):
-			prev_line_no = line_no
-			line_no += len(re.findall("\n", token))
+        for token in tags_re.split(rendered):
+            prev_line_no = line_no
+            line_no += len(re.findall("\n", token))
 
-			if token.startswith(VARIABLE_TAG_START):
-				start_l = len(VARIABLE_TAG_START)
-				end_l = len(VARIABLE_TAG_END)
-				token = Token(token[start_l:-end_l].strip(), line_no=line_no)
-			elif VARIABLE_TAG_START in token and VARIABLE_TAG_END in token:
-				tiny_line_no = 0
-				# TODO: I might want to save that in a list for
-				# a later use. Or refactor `token` into the list
-				# type, and instead of .append use tokens + token syntax.
-				for i, tiny in enumerate(re.split(r"\n", token)):
-					if VARIABLE_TAG_START in tiny:
-						tiny_line_no = i
-						raise create_exception(f"Line {prev_line_no + tiny_line_no}: new line after the opening variable tag (variable tags must be defined in a single line)")
-				raise create_exception(f"Lines {prev_line_no} to {line_no}: new line after opening variable tag (variable tags must be defined in a single line)")
-			elif VARIABLE_TAG_START in token:
-				"""Variable opening tag found, but not parsed, may be opened and not closed variable tag."""
-				raise create_exception(f"Line {line_no}: not closed variable tag")
-			elif VARIABLE_TAG_END in token:
-				raise create_exception(f"Line {line_no}: single closed variable tag (did you forget to open variable tag?)")
-			tokens.append(token)
-		return tokens
+            if token.startswith(VARIABLE_TAG_START):
+                start_l = len(VARIABLE_TAG_START)
+                end_l = len(VARIABLE_TAG_END)
+                token = Token(token[start_l:-end_l].strip(), line_no=line_no)
+            elif VARIABLE_TAG_START in token and VARIABLE_TAG_END in token:
+                tiny_line_no = 0
+                # TODO: I might want to save that in a list for
+                # a later use. Or refactor `token` into the list
+                # type, and instead of .append use tokens + token syntax.
+                for i, tiny in enumerate(re.split(r"\n", token)):
+                    if VARIABLE_TAG_START in tiny:
+                        tiny_line_no = i
+                        raise create_exception(f"Line {prev_line_no + tiny_line_no}: new line after the opening variable tag (variable tags must be defined in a single line)")
+                raise create_exception(f"Lines {prev_line_no} to {line_no}: new line after opening variable tag (variable tags must be defined in a single line)")
+            elif VARIABLE_TAG_START in token:
+                """Variable opening tag found, but not parsed, may be opened and not closed variable tag."""
+                raise create_exception(f"Line {line_no}: not closed variable tag")
+            elif VARIABLE_TAG_END in token:
+                raise create_exception(f"Line {line_no}: single closed variable tag (did you forget to open variable tag?)")
+            tokens.append(token)
+        return tokens
 
-	def render(self):
-		"""Render a template string."""
-		self.tokens = self.tokenize()
+    def render(self):
+        """Render a template string."""
+        self.tokens = self.tokenize()
 
-		return "".join([self.process_token(t) for t in self.tokens])
+        return "".join([self.process_token(t) for t in self.tokens])
 
-	@classmethod
-	def from_string(cls, template, context=None):
-		"""Initialize template from string
+    @classmethod
+    def from_string(cls, template, context=None):
+        """Initialize template from string
 
-		context is a dictionary containing your variables, by default
-		it is an empty dictionary
+        context is a dictionary containing your variables, by default
+        it is an empty dictionary
 
-		Sample:
-		>>> Template.from_string(template_string, {'variable': 'value'})
-		"""
-		if not context:
-			context = {}
-		return cls(template, context)
+        Sample:
+        >>> Template.from_string(template_string, {'variable': 'value'})
+        """
+        if not context:
+            context = {}
+        return cls(template, context)
 
 
 class Token:
-	"""Represents an inline token.
+    """Represents an inline token.
 
-	After parsing a template string, the Template class will create tokens to
-	represent parts that require additional processing.
+    After parsing a template string, the Template class will create tokens to
+    represent parts that require additional processing.
 
-	Types of tokens:
-	VARIABLE - represents a variable token, for example, `<<VAR>>` string would translate
-	into a Token instance with a VARIABLE type. Variable tags inside template strings must be defined in a
-	single line.
-	"""
+    Types of tokens:
+    VARIABLE - represents a variable token, for example, `<<VAR>>` string would translate
+    into a Token instance with a VARIABLE type. Variable tags inside template strings must be defined in a
+    single line.
+    """
 
-	def __init__(self, key, line_no):
-		"""Creates a new token
+    def __init__(self, key, line_no):
+        """Creates a new token.  
 
-		key is the name of the variable that the `Template.render()` method parsed from a template string
+        key is the name of the variable that the `Template.render()` method parsed from a template string
 
-		line_no is a number of a line at which token tags were discovered
-		"""
-		self.key = key
-		self.line_no = line_no
+        line_no is a number of a line at which token tags were discovered
+        """
+        self.key = key
+        self.line_no = line_no
 
-		# The default attribute, for now, is the dictionary
-		# of callables that provide default values.
-		self.defaults = DEFAULT_VARIABLE_REGISTRY
+        # The default attribute, for now, is the dictionary
+        # of callables that provide default values.
+        self.defaults = DEFAULT_VARIABLE_REGISTRY
 
-	def render(self, context):
-		"""Use actual values from the Template's context to render a token.
+    def render(self, context):
+        """Use actual values from the Template's context to render a token.
 
-		Every token represents a single variable from a template string. Hence it
-		is a relatively simple operation. All we have to do here is to return the value of 
-		`self.key` key of the `context` dictionary.
-		"""
-		if len(self.key) == 0:
-			raise create_exception(f"Line {self.line_no}: empty token variable on line")
+        Every token represents a single variable from a template string. Hence it
+        is a relatively simple operation. All we have to do here is to return the value of 
+        `self.key` key of the `context` dictionary.
+        """
+        if len(self.key) == 0:
+            raise create_exception(f"Line {self.line_no}: empty token variable on line")
 
-		if self.is_string_statement():
-			"""If the `self.key` is the string, i.e., template string for that fragment could look like: <<"STR">>."""
-			q = self.key[0]
-			stripped = self.key.lstrip(q).rstrip(q)
-			if q in stripped:
-				"""This case is when the variable tag contains two strings, or an incorrect string."""
-				raise create_exception(f"Line {self.line_no}: incorrect string declaration, you can have only one string in the variable tag")
-			return stripped
+        if self.is_string_statement():
+            """If the `self.key` is the string, i.e., template string for that fragment could look like: <<"STR">>."""
+            q = self.key[0]
+            stripped = self.key[1:-1]
+            if q in stripped:
+                """When the variable tag contains two strings, or an incorrect string."""
+                raise create_exception(f"Line {self.line_no}: incorrect string declaration, you can have only one string in the variable tag")
+            return stripped
 
-		if len(self.key) <= 2:
-			raise create_exception(f"Line {self.line_no}: the variable name is too short; variable names should be at leas 3 characters long")
+        if len(self.key) <= 2:
+            raise create_exception(f"Line {self.line_no}: the variable name is too short; variable names should be at leas 3 characters long")
 
-		if not self.key.isidentifier():
-			raise create_exception(f"Line {self.line_no}: incorrect variable name `{self.key}`")
+        if not self.key.isidentifier():
+            raise create_exception(f"Line {self.line_no}: incorrect variable name `{self.key}`")
 
-		if self.key.startswith("D") and self.key[1:] in self.defaults:
-			"""Default variables start with the `D` prefix.
-			TODO: If there is a variable in the context dictionary under the `self.key` key then use that one.
-			"""
-			return self.defaults[self.key[1:]]()
+        if self.key.startswith("D") and self.key[1:] in self.defaults:
+            """Default variables start with the `D` prefix.
+            TODO: If there is a variable in the context dictionary under the `self.key` key then use that one.
+            """
+            return self.defaults[self.key[1:]]()
 
-		if self.key not in context:
-			raise create_exception(f"Line {self.line_no}: the variable `{self.key}` is not defined in the context dictionary", token=self, exception_class=TemplateKeyError)
+        if self.key not in context:
+            raise create_exception(f"Line {self.line_no}: the variable `{self.key}` is not defined in the context dictionary", token=self, exception_class=TemplateKeyError)
 
-		return context[self.key]
+        return context[self.key]
 
-	def is_string_statement(self):
-		"""Checks if the `self.key` contained between quotes."""
-		if self.key.startswith("\"") and self.key.endswith("\""):
-			return True
-		elif self.key.startswith("'") and self.key.endswith("'"):
-			return True
-		return False
+    def is_string_statement(self):
+        """Checks if the `self.key` contained between quotes."""
+        if self.key.startswith("\"") and self.key.endswith("\""):
+            return True
+        elif self.key.startswith("'") and self.key.endswith("'"):
+            return True
+        return False
 
-	def __str__(self):
-		return self.key
+    def __str__(self):
+        return self.key
--- tests/test_base.py
+++ tests/test_base.py
@@ -11,108 +11,109 @@ from tempearly.exceptions import TemplateKeyError, TemplateSyntaxError
 
 
 def test_no_tags():
-	"""Test simple template strings without any tags.
+    """Test simple template strings without any tags.
 
-	The Template.render() method should return the same string it was initialize with.
-	"""
-	template_strings = [
-		"simple template",
-		"<p>hello there</p>",
-		"<html><div></div></html>"
-	]
+    The Template.render() method should return the same string it was initialize with.
+    """
+    template_strings = [
+        "simple template",
+        "<p>hello there</p>",
+        "<html><div></div></html>"
+    ]
 
-	for ts in template_strings:
-		template = Template.from_string(ts)
-		assert template.render() == ts
-		assert len(template.tokens) > 0
+    for ts in template_strings:
+        template = Template.from_string(ts)
+        assert template.render() == ts
+        assert len(template.tokens) > 0
 
 
 def test_variables():
-	"""Test template strings with simple variable tokens
+    """Test template strings with simple variable tokens
 
-	The Template.render() method should replace these tokens with
-	appropriate values. If the contents within variable tag are put
+    The Template.render() method should replace these tokens with
+    appropriate values. If the contents within variable tag are put
     between quotes, then that tag should evaluate to that string.
-	"""
-	template_strings = [
-		("<<VAR>>", {"VAR": 12,}, "12"),
-		("<div><<VAR>></div>", {"VAR": 1,}, "<div>1</div>"),
-		("""<div><< VAR >></div>""", {"VAR": 1,}, "<div>1</div>"),
-		("""<div><< "VAR" >></div>""", {"VAR": 1,}, "<div>VAR</div>"),
-		("""<div><< " VAR" >></div>""", {"VAR": 1,}, "<div> VAR</div>"),
-		("""<div><< " " >></div>""", {"VAR": 1,}, "<div> </div>"),
-		("""<div><< "" >></div>""", {"VAR": 1,}, "<div></div>"),
-	]
+    """
+    template_strings = [
+        ("<<VAR>>", {"VAR": 12,}, "12"),
+        ("<div><<VAR>></div>", {"VAR": 1,}, "<div>1</div>"),
+        ("""<div><< VAR >></div>""", {"VAR": 1,}, "<div>1</div>"),
+        ("""<div><< "VAR" >></div>""", {"VAR": 1,}, "<div>VAR</div>"),
+        ("""<div><< " VAR" >></div>""", {"VAR": 1,}, "<div> VAR</div>"),
+        ("""<div><< ' VAR' >></div>""", {"VAR": 1,}, "<div> VAR</div>"),
+        ("""<div><< " " >></div>""", {"VAR": 1,}, "<div> </div>"),
+        ("""<div><< "" >></div>""", {"VAR": 1,}, "<div></div>"),
+    ]
 
-	for ts in template_strings:
-		template = Template.from_string(ts[0], context=ts[1])
-		assert template.render() == ts[-1]
+    for ts in template_strings:
+        template = Template.from_string(ts[0], context=ts[1])
+        assert template.render() == ts[-1]
 
-	# A variable tag with two string declarations should raise an exception.
-	t = Template.from_string("<<'' ''>>")
-	with pytest.raises(TemplateSyntaxError) as e:
-		t.render()
+    # A variable tag with two string declarations should raise an exception.
+    t = Template.from_string("<<'' ''>>")
+    with pytest.raises(TemplateSyntaxError) as e:
+        t.render()
 
 
 def test_incorrect_tags():
-	"""Tests that a Template object can handle incorrect template strings."""
-	# (template string, context, part of an expected exception)
-	template_strings = [
-		("<div><<>></div>", {}),
-		("<div><<</div>", {}),
-		("<div>>></div>", {}),
-		("<div><<<VAR>></div>", {}),
-		("""<div>
-			<<<VAR>></div>""", {}),
-		("""<div>
+    """Tests that a Template object can handle incorrect template strings."""
+    # (template string, context, part of an expected exception)
+    template_strings = [
+        ("<div><<>></div>", {}),
+        ("<div><<</div>", {}),
+        ("<div>>></div>", {}),
+        ("<div><<<VAR>></div>", {}),
+        ("""<div>
+            <<<VAR>></div>""", {}),
+        ("""<div>
 
-			<<<VAR>></div>""", {}),
-		("""<div>
+            <<<VAR>></div>""", {}),
+        ("""<div>
 
-			<<
-
-
-
-			<VAR>></div>""", {}),
-		("""<div><<
+            <<
 
 
 
-			<VAR>></div>""", {}),
-	]
+            <VAR>></div>""", {}),
+        ("""<div><<
 
-	exception_msgs = [
-		["Line 1"],
-		["Line 1", "not closed"],
-		["Line 1", "forget"],
-		["Line 1"],
-		["Line 2"],
-		["Line 3"],
-		["Line 3", "must", "single", "line"],
-		["Line 1", "must", "single", "line"],
-	]
 
-	for i, ts in enumerate(template_strings):
-		template = Template.from_string(ts[0], context=ts[1])
 
-		with pytest.raises(TemplateSyntaxError) as e:
-			template.render()
-		for m in exception_msgs[i]:
-			assert m in str(e), f"Testing index: {i}"
+            <VAR>></div>""", {}),
+    ]
+
+    exception_msgs = [
+        ["Line 1"],
+        ["Line 1", "not closed"],
+        ["Line 1", "forget"],
+        ["Line 1"],
+        ["Line 2"],
+        ["Line 3"],
+        ["Line 3", "must", "single", "line"],
+        ["Line 1", "must", "single", "line"],
+    ]
+
+    for i, ts in enumerate(template_strings):
+        template = Template.from_string(ts[0], context=ts[1])
+
+        with pytest.raises(TemplateSyntaxError) as e:
+            template.render()
+        for m in exception_msgs[i]:
+            assert m in str(e), f"Testing index: {i}"
 
 
 def test_variable_does_not_exist():
-	"""A variable name used in the variable tags must be defined in the context."""
-	templates = ("<<div>>", {}) # Notice there is no "div" key in the dictionary.
-	template = Template.from_string(templates[0], templates[1])
-	with pytest.raises(TemplateKeyError) as e:
-		template.render()
-	assert "context" in str(e)
-	assert "Line 1" in str(e)
-	try:
-		template.render()
-	except TemplateKeyError as e:
-		assert e.token
+    """A variable name used in the variable tags must be defined in the context."""
+    templates = ("<<div>>", {}) # Notice there is no "div" key in the dictionary.
+    template = Template.from_string(templates[0], templates[1])
+    with pytest.raises(TemplateKeyError) as e:
+        template.render()
+    assert "context" in str(e)
+    assert "Line 1" in str(e)
+    try:
+        template.render()
+    except TemplateKeyError as e:
+        assert e.token
 
 
 def test_default_variables():
